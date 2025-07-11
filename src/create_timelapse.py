ef762c37745e4f70e49f296d2315bba43104d1f0
karthiks1701 <soma.nitt@gmail.com>
2025-07-10 20:07:05 -0400

    some modifications

--- src/create_timelapse.py
+++ src/create_timelapse.py
@@ -7,11 +7,31 @@ from pathlib import Path
 import cv2
 import torch
 from groundingdino.util.inference import load_model, load_image, predict, annotate
-from utils import get_config, make_frames
+from utils import (
+    get_config,
+    make_frames,
+    get_grounding_output,
+    intersection_area,
+    grabcut_mask_inside_box,
+    get_spline,
+)
 import supervision as sv
+import numpy as np
 import yaml
 import ffmpeg
 import argparse
+
+# from segment_anything import sam_model_registry, SamPredictor
+
+# sam_checkpoint = "/home/somz/projects/TimelapseFromPrompt/weights/sam_vit_h_4b8939.pth"
+# model_type = "vit_h"
+
+# device = "cpu"
+
+# sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+# sam.to(device=device)
+
+# predictor = SamPredictor(sam)
 
 if __name__ == "__main__":
 
@@ -23,6 +43,7 @@ if __name__ == "__main__":
     model = load_model(
         f"{file_path}/config_dino.py",
         f"{file_path}/../weights/groundingdino_swint_ogc.pth",
+        device="cpu",
     )
 
     # Get the config files from
@@ -34,9 +55,9 @@ if __name__ == "__main__":
         algo = config
         video_path = f"{file_path}/../{configs[config]['video_dir']}"
         frames_path = f"{file_path}/../frames/{algo}/"
-        print(video_path)
         output_path = f"{algo}.png"
         fps = configs[config]["fps"]
+        token_spans = configs[config]["token_spans"]
         make_frames(input_video=video_path, frames_path=frames_path, fps=fps)
 
         # === Load Grounding DINO model ===
@@ -56,13 +77,15 @@ if __name__ == "__main__":
             reverse=False,
         )
 
+        print(frame_files)
+
         # === Load first frame as background using OpenCV ===
         background_path = os.path.join(frames_path, frame_files[0])
         background_bgr = cv2.imread(background_path)
         robot_overlay_discrete = background_bgr.copy()
-
+        points = []
         total_images = len(frame_files)
-
+        prev_box = None
         change_index = int(2 * total_images // 3)  # final third
 
         # === Process each frame ===
@@ -70,7 +93,7 @@ if __name__ == "__main__":
             # increase frame interval near the end
             if idx == change_index:
                 frame_interval = configs[config]["frame_interval_end"]
-            if idx % frame_interval != 0:
+            if idx % frame_interval != 0 and idx != total_images - 1:
                 continue
 
             img_path = os.path.join(frames_path, filename)
@@ -82,26 +105,30 @@ if __name__ == "__main__":
             frame_raw, frame = load_image(img_path)
 
             # Grounding DINO expects RGB (0-255)
-            boxes, logits, phrases = predict(
+            # boxes, logits, phrases = predict(
+            #     model=model,
+            #     image=frame,
+            #     caption=TEXT_PROMPT,
+            #     box_threshold=BOX_THRESHOLD,
+            #     text_threshold=TEXT_THRESHOLD,
+            # )
+
+            boxes_filt, pred_phrases = get_grounding_output(
                 model=model,
                 image=frame,
                 caption=TEXT_PROMPT,
                 box_threshold=BOX_THRESHOLD,
                 text_threshold=TEXT_THRESHOLD,
+                cpu_only=False,
+                token_spans=eval(f"{token_spans}"),
             )
+
             H, W, _ = frame_bgr.shape
 
-            # annotated_frame = annotate(image_source=frame_bgr, boxes=boxes, logits=logits, phrases=phrases)
+            if boxes_filt.shape[0] != 0:
 
-            # sv.plot_image(annotated_frame, (16, 16))
-            # # show image
-            # # cv2.imshow(f"Annotated Frame {idx}", annotated_frame)
-            # # cv2.waitKey(10)  # Allow OpenCV to update the window
-            # # cv2.destroyAllWindows()
-
-            if boxes.shape[0] > 0:
-                max_idx = torch.argmax(logits).item()
-                cx, cy, w, h = boxes[max_idx].tolist()
+                print(idx, boxes_filt, pred_phrases)
+                cx, cy, w, h = boxes_filt[0].tolist()
                 # Convert from center format to top-left/bottom-right
                 x0 = int((cx - w / 2) * W)
                 y0 = int((cy - h / 2) * H)
@@ -114,11 +141,83 @@ if __name__ == "__main__":
                 y0 = max(0, min(y0, H - 1))
                 y1 = max(0, min(y1, H - 1))
 
+                iou = 0
+                if prev_box is not None:
+                    area_a = (x1 - x0) * (y1 - y0)
+                    x0_p, y0_p, x1_p, y1_p = prev_box
+                    area_b = (x1_p - x0_p) * (y1_p - y0_p)
+                    iou = intersection_area((x0, y0, x1, y1), prev_box) / (
+                        area_a + area_b - intersection_area((x0, y0, x1, y1), prev_box)
+                    )
+
+                print(iou)
+                if iou > 0.05:
+                    print("very near old detection")
+                    continue
+
+                print("patching")
+                # annotated_frame = frame_bgr.copy()
+                # cv2.rectangle(annotated_frame, (x0, y0), (x1, y1), (0, 255, 0), 2)
+                # annotated_frame = annotate(
+                #     image_source=frame_bgr, boxes=boxes_filt, logits=None, phrases=None
+                # )
+                # annotated_frame = annotate(image_source=frame_bgr, boxes=boxes, logits=logits, phrases=phrases)
+
+                # sv.plot_image(annotated_frame, (16, 16))
+                # # show image
+                # cv2.imshow(f"Annotated Frame {idx}", annotated_frame)
+                # cv2.waitKey(300)  # Allow OpenCV to update the window
+                # cv2.destroyAllWindows()
+
                 # Crop from the current frame and paste onto the overlay
+                # overlay an arrow
+
                 patch = frame_bgr[y0:y1, x0:x1]
+
+                # input_box = boxes_filt[0].cpu().numpy()
+                # predictor.set_image(frame_bgr)
+                # masks, _, _ = predictor.predict(
+                #     point_coords=None,
+                #     point_labels=None,
+                #     box=input_box[None, :],
+                #     multimask_output=False,
+                # )
+
+                # # Apply mask to image (keep only the masked area, rest black
+                # masked_image = cv2.bitwise_and(frame_bgr, frame_bgr, mask=masks[0])
+                # cv2.imshow("remove background", masked_image)
+                # cv2.waitKey(0)
+                # cv2.destroyAllWindows
+                # patch = grabcut_mask_inside_box(img=frame_bgr, box=(x0, y0, x1, y1))
                 if patch.shape[0] > 0 and patch.shape[1] > 0:
                     robot_overlay_discrete[y0:y1, x0:x1] = patch
 
-            # === Save result ===
+                points.append((int((x0 + x1) / 2), int(y1 - 100)))
+
+                prev_box = (x0, y0, x1, y1)
+
+        print(points)
+        print(H, W)
+        spline_points = get_spline(points)
+        print(spline_points.shape)
+
+        # cv2.polylines(
+        #     robot_overlay_discrete,
+        #     points,
+        #     isClosed=False,
+        #     color=(255, 0, 0),
+        #     thickness=2,
+        # )
+        cv2.polylines(
+            robot_overlay_discrete,
+            [spline_points],
+            isClosed=False,
+            color=(255, 0, 0),
+            thickness=2,
+        )
+
+        cv2.imwrite("debug.png", background_bgr)
+
+        # === Save result ===
         cv2.imwrite(output_path, robot_overlay_discrete)
         print(f"✅ Saved overlay result to: {output_path}")
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,16 @@ utils for the main file
 
 import yaml
 import ffmpeg
+import torch
+import shutil
 import os
+from groundingdino.util import box_ops
+from groundingdino.util.slconfig import SLConfig
+from groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
+from groundingdino.util.vl_utils import create_positive_map_from_span
+from scipy.interpolate import splprep, splev
+import numpy as np
+import cv2
 
 
 def get_config(config_path: str = None):
@@ -29,7 +38,137 @@ def make_frames(input_video: str, frames_path: str, fps: int):
     """
     use ffmpeg to get the frames
     """
-    output_pattern = os.path.join(frames_path, "frame_%04d.png")
+    output_pattern = os.path.join(frames_path, "%04d.png")
+    # delete only if it exists
+    shutil.rmtree(frames_path, ignore_errors=True)
     os.makedirs(frames_path, exist_ok=True)
 
     (ffmpeg.input(input_video).filter("fps", fps=fps).output(output_pattern).run())
+
+
+def intersection_area(boxA, boxB):
+    x1_a, y1_a, x2_a, y2_a = boxA
+    x1_b, y1_b, x2_b, y2_b = boxB
+
+    x_left = max(x1_a, x1_b)
+    y_top = max(y1_a, y1_b)
+    x_right = min(x2_a, x2_b)
+    y_bottom = min(y2_a, y2_b)
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0
+
+    return (x_right - x_left) * (y_bottom - y_top)
+
+
+def grabcut_mask_inside_box(img, box):
+    x1, y1, x2, y2 = map(int, box)
+    crop = img[y1:y2, x1:x2].copy()
+
+    mask = np.zeros(crop.shape[:2], np.uint8)
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+
+    rect = (5, 5, crop.shape[1] - 10, crop.shape[0] - 10)  # slight inset
+
+    cv2.grabCut(crop, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    final_mask = np.where((mask == 2) | (mask == 0), 0, 1).astype("uint8")
+
+    crop[final_mask == 0] = 0  # remove background
+
+    return crop
+
+
+def get_grounding_output(
+    model,
+    image,
+    caption,
+    box_threshold,
+    text_threshold=None,
+    with_logits=True,
+    cpu_only=False,
+    token_spans=None,
+):
+    assert (
+        text_threshold is not None or token_spans is not None
+    ), "text_threshould and token_spans should not be None at the same time!"
+    caption = caption.lower()
+    caption = caption.strip()
+    if not caption.endswith("."):
+        caption = caption + "."
+    device = "cuda" if not cpu_only else "cpu"
+    model = model.to(device)
+    image = image.to(device)
+    with torch.no_grad():
+        outputs = model(image[None], captions=[caption])
+    logits = outputs["pred_logits"].sigmoid()[0]  # (nq, 256)
+    boxes = outputs["pred_boxes"][0]  # (nq, 4)
+
+    # filter output
+    if token_spans is None:
+        logits_filt = logits.cpu().clone()
+        boxes_filt = boxes.cpu().clone()
+        filt_mask = logits_filt.max(dim=1)[0] > box_threshold
+        logits_filt = logits_filt[filt_mask]  # num_filt, 256
+        boxes_filt = boxes_filt[filt_mask]  # num_filt, 4
+
+        # get phrase
+        tokenlizer = model.tokenizer
+        tokenized = tokenlizer(caption)
+        # build pred
+        pred_phrases = []
+        for logit, box in zip(logits_filt, boxes_filt):
+            pred_phrase = get_phrases_from_posmap(
+                logit > text_threshold, tokenized, tokenlizer
+            )
+            if with_logits:
+                pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
+            else:
+                pred_phrases.append(pred_phrase)
+    else:
+        # given-phrase mode
+        positive_maps = create_positive_map_from_span(
+            model.tokenizer(caption), token_span=token_spans
+        ).to(
+            image.device
+        )  # n_phrase, 256
+
+        logits_for_phrases = positive_maps @ logits.T  # n_phrase, nq
+        all_logits = []
+        all_phrases = []
+        all_boxes = []
+        for token_span, logit_phr in zip(token_spans, logits_for_phrases):
+            # get phrase
+            phrase = " ".join([caption[_s:_e] for (_s, _e) in token_span])
+            # get mask
+            filt_mask = logit_phr > box_threshold
+            # filt box
+            all_boxes.append(boxes[filt_mask])
+            # filt logits
+            all_logits.append(logit_phr[filt_mask])
+            if with_logits:
+                logit_phr_num = logit_phr[filt_mask]
+                all_phrases.extend(
+                    [phrase + f"({str(logit.item())[:4]})" for logit in logit_phr_num]
+                )
+            else:
+                all_phrases.extend([phrase for _ in range(len(filt_mask))])
+        boxes_filt = torch.cat(all_boxes, dim=0).cpu()
+        pred_phrases = all_phrases
+
+    return boxes_filt, pred_phrases
+
+
+def get_spline(points):
+
+    points = np.array(points)
+
+    # Fit a spline
+    tck, u = splprep(points.T, s=0)
+    u_fine = np.linspace(0, 1, 100)
+    x_fine, y_fine = splev(u_fine, tck)
+
+    # Prepare points for OpenCV
+    smooth_pts = np.array(list(zip(x_fine, y_fine)), dtype=np.int32)
+    smooth_pts = smooth_pts.reshape((-1, 1, 2))
+    return smooth_pts
